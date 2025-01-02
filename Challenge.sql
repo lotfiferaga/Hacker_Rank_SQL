532f03dc1223a8268e1cef9fae5986b8e95ca8b2
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2025-01-02 21:16:26 +0100

    Update Challenge.sql

--- Challenge.sql
+++ Challenge.sql
@@ -19,7 +19,8 @@ order by RIGHT(Name,3),ID asc
 select name from Employee order by name asc;
 
 -- 
-
+select name from Employee where salary > 2000 and months < 10 
+order by employee_id asc;
 
 
 
