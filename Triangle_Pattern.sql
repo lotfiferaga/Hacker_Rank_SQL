6accdbe457473199ef8fe1c52b54510eb53b08a5
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2025-02-05 23:04:44 +0100

    Update Triangle_Pattern.sql

--- Triangle_Pattern.sql
+++ Triangle_Pattern.sql
@@ -1,6 +1,18 @@
+-- this is for mysql
+
 WITH RECURSIVE pattern AS (
     SELECT 20 AS n
     UNION ALL
     SELECT n - 1 FROM pattern WHERE n > 1
 )
 SELECT REPEAT('* ', n) FROM pattern;
+
+
+-- reverse triangle
+WITH RECURSIVE Pattern AS
+(
+    SELECT 1 AS n 
+    UNION ALL
+    SELECT n + 1 FROM Pattern WHERE n < 20
+)
+SELECT REPEAT('* ',n) FROM pattern;
