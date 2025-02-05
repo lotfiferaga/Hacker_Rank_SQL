eb328d7a34da948e9453fb4113e3236e5fed5016
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2025-02-05 23:05:43 +0100

    Update Triangle_Pattern.sql

--- Triangle_Pattern.sql
+++ Triangle_Pattern.sql
@@ -16,3 +16,15 @@ WITH RECURSIVE Pattern AS
     SELECT n + 1 FROM Pattern WHERE n < 20
 )
 SELECT REPEAT('* ',n) FROM pattern;
+
+
+-- for SQL SERVER
+WITH Pattern AS 
+(
+    -- Start with n = 1
+    SELECT 1 AS n  
+    UNION ALL  
+    -- Increment n by 1 until it reaches 20
+    SELECT n + 1 FROM Pattern WHERE n < 20  
+)
+SELECT REPLICATE('* ', n) FROM Pattern; -- Repeat '* ' n times
